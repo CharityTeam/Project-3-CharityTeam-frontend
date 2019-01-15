ff19f46d6e9ba5b6c6fb6e8ec7abd3ce089fe993
rawandahli <rawandahlii@gmail.com>
2019-01-15 15:51:12 +0300

    done with the onecase

--- charity_app/src/components/OneCase.js
+++ charity_app/src/components/OneCase.js
@@ -1,9 +1,31 @@
 import React from "react";
 
 const OneCase = props => {
-    return(
-        <div>One Case</div>
-    )
+    return (
+        <div>
+            <div className="case"
+            onClick={() => {
+                props.setCurrentCase(props.activeCase);
+            }}>
+
+                <div className="image">
+                    <img>src={props.activeCase.img} alt ="" /></img>
+                </div>
+
+                <h2>{props.activeCase.name}</h2>
+                <h2>{props.activeCase.details}</h2>
+                <h2>{props.activeCase.sum}</h2>
+                <h2>{props.activeCase.needed}</h2>
+                <h2>{props.activeCase.number_of_donors}</h2>
+
+                <div className="buttons">
+                    <button onClick={() => { props.toggleModal() }}>donate</button>
+                </div>
+            </div> 
+        </div>
+    );
 };
+
+
 
 export default OneCase;
