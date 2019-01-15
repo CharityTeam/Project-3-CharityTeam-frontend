ff82b0a72ee39de43c5f64b51b2d0d07e555a74a
SultanBS <alsubaie.sultan.b@gmail.com>
2019-01-15 17:41:04 +0300

    Editing and debugging

--- charity_app/src/components/OneCase.js
+++ charity_app/src/components/OneCase.js
@@ -1,25 +1,26 @@
 import React from "react";
 
-const OneCase = props => {
+const OneCase = (props) => {
     return (
         <div>
-            <div className="case"
+            <div className="back"
             onClick={() => {
-                props.setCurrentCase(props.activeCase);
-            }}>
-
+                props.setCurrentCase(null);
+            }}> Back </div>
+                <div className="container">
                 <div className="image">
-                    <img>src={props.activeCase.img} alt ="" /></img>
+                    <img src={props.activeCase.img} alt=""/>
                 </div>
 
                 <h2>{props.activeCase.name}</h2>
                 <h2>{props.activeCase.details}</h2>
                 <h2>{props.activeCase.sum}</h2>
                 <h2>{props.activeCase.needed}</h2>
-                <h2>{props.activeCase.number_of_donors}</h2>
+                <h2>{props.activeCase.numberofdonors}</h2>
 
                 <div className="buttons">
-                    <button onClick={() => { props.toggleModal() }}>donate</button>
+                    <button onClick={() => {props.toggleModal() }}>Edit</button>
+                    <button onClick={() => {props.deleteCase(props.activeCase.id)}}>Delete</button>
                 </div>
             </div> 
         </div>
