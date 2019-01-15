a7be667d6fab1dc64bd6819d65e8abcad5fced03
jalharbi <jumanah.alharbi92@gmail.com>
2019-01-15 14:52:22 +0300

    finish form-page

--- charity_app/src/components/CaseForm.js
+++ charity_app/src/components/CaseForm.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 
 class CaseForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.activeCase ? props.activeCase.name : '',
+            details: props.activeCase ? props.activeCase.details : '',
+            city: props.activeCase ? props.activeCase.city : '',
+            email: props.activeCase ? props.activeCase.email : '',
+            phone: props.activeCase ? props.activeCase.phone : '',
+            organtion_name: props.activeCase ? props.activeCase.organtion_name : '',
+            needed: props.activeCase ? props.activeCase.needed : '',
+            img: props.activeCase ? props.activeCase.img : '',
+            sum: props.activeCase ? props.activeCase.sum : '',
+        }
+    }
+
+    handleChange(event) {
+        const currentInput = event.target.name;
+        const newValue = event.target.value;
+        console.log('current input: ', currentInput);
+        console.log('newValue: ', newValue);
 
 
-    render(){
-        return(
-            <div>Case Form</div>
+        this.setState({
+            [currentInput]: newValue
+        }, function () {
+            console.log(this.state);
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.handleSubmit(this.state)
+    }
+
+    render() {
+        return (
+            <div className='modal'>
+                <form className='show-form' onSubmit={this.handleSubmit.bind(this)}>
+                    {/* <div className='close-modal' onClick={() => { this.props.toggleModal() }}>delete</div> */}
+                    <label>name:</label><input type="text" value={this.state.name} name="name" onChange={this.handleChange.bind(this)} /><br />
+                    <label>details</label><input type="text" value={this.state.details} name="details" onChange={this.handleChange.bind(this)} /><br />
+                    <label>city:</label><input type="text" value={this.state.city} name="city" onChange={this.handleChange.bind(this)} /><br />
+                    <label>email:</label><input type="text" value={this.state.email} name="email" onChange={this.handleChange.bind(this)} /><br />
+                    <label>phone:</label><input type="text" value={this.state.phone} name="phone" onChange={this.handleChange.bind(this)} /><br />
+                    <label>organtion_name:</label><input type="text" value={this.state.organtion_name} name="organization_name" onChange={this.handleChange.bind(this)} /><br />
+                    <label>needed:</label><input type="text" value={this.state.needed} name="needed" onChange={this.handleChange.bind(this)} /><br />
+                    <label>img:</label><input type="text" value={this.state.img} name="img" onChange={this.handleChange.bind(this)} /><br />
+                    <label>sum:</label><input type="text" value={this.state.sum} name="sum" onChange={this.handleChange.bind(this)} /><br />
+                </form>
+
+
+            </div>
         )
     }
 };
